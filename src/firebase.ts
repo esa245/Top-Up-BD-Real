90211a11eb3ad5e8d20a5ab2c698e358e85c8210
esa245 <mdesaalli74@gmail.com>
2026-03-30 18:45:50 +0600

    feat: Update Firebase config
    Switch to new Firebase project configuration for Top Up BD.

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmZMSZEqZbgjY6q9rBp1N4FJDqa20T6WE",
-  authDomain: "count-time-7e879.firebaseapp.com",
-  databaseURL: "https://count-time-7e879-default-rtdb.firebaseio.com",
-  projectId: "count-time-7e879",
-  storageBucket: "count-time-7e879.firebasestorage.app",
-  messagingSenderId: "38354668085",
-  appId: "1:38354668085:web:2454db9219b908acbc3f7c",
-  measurementId: "G-J12WRP3LEE"
+  apiKey: "AIzaSyBtA6IuG9Z74tT1JQwmRMp9LrXXh4i2Zqw",
+  authDomain: "rn-incam.firebaseapp.com",
+  databaseURL: "https://rn-incam-default-rtdb.firebaseio.com",
+  projectId: "rn-incam",
+  storageBucket: "rn-incam.firebasestorage.app",
+  messagingSenderId: "811957444320",
+  appId: "1:811957444320:web:8ced488b713f71a3c67a85"
 };
 
 const app = initializeApp(firebaseConfig);
